887210e29b44db4496bcd41e2cf291ee4146baea
vitor nascimento <nascimento.vitor@academico.ifpb.edu.br>
2022-01-15 21:54:29 -0300

    Capturar elementos htlm manipulaveis

---
+++ script.js
@@ -0,0 +1,25 @@
+/**
+ * @getElementsByClassName = Esta função irá pegar todas as imagens com a classe "imagem-para-alterar"
+ * @Valor - Esta constante terá um valor de um array de 3 posições [0,1,2], sendo uma imagem diferente pra cada uma delas.
+ */
+ const imagem = document.body.getElementsByClassName("imagem-para-alterar");
+
+ /**
+  * @getElementsByClassName = Esta função irá pegar todas as imagens com a classe "nome-do-personagem"
+  * @Valor - Esta constante terá um valor de um array de 3 posições [0,1,2], sendo um campo "nome" diferente pra cada uma delas.
+  */
+ const nome = document.body.getElementsByClassName("nome-do-personagem");
+ 
+ /**
+  * @getElementsByClassName = Esta função irá pegar todas as imagens com a classe "especie-do-personagem"
+  * @Valor - Esta constante terá um valor de um array de 3 posições [0,1,2], sendo um campo "especie" diferente pra cada uma delas.
+  */
+ const especie = document.body.getElementsByClassName("especie-do-personagem");
+ 
+ /**
+  * @getElementsByClassName = Esta função irá pegar todas as imagens com a classe "status-do-personagem"
+  * @Valor - Esta constante terá um valor de um array de 3 posições [0,1,2], sendo um campo "status" diferente pra cada uma delas.
+  */
+ const condicao = document.body.getElementsByClassName("status-do-personagem");
+
+ 
